f4f285a49483938b6957f1b7021a0993d83473f4
Vitória Toebe <vitoriatoebe@gmail.com>
2021-12-05 01:20:57 -0300

    create ContinuityChecker class
    Checks if the user would like the program to keep running

---
+++ src/com/vtoebe/temperature_converter/utils/ContinuityChecker.java
@@ -0,0 +1,7 @@
+package com.vtoebe.temperature_converter.utils;
+
+public class ContinuityChecker {
+    public boolean checkContinuity(char willContinue){
+        return willContinue == 'Y';
+    }
+}
